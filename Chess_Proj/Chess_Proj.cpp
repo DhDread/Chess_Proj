29246076e8db13ebdba438e7dc7302085ef6cfe8
DANIEL DRISCOLL <3700106@Mobis.HKMG.Global>
2018-01-08 18:02:05 -0500

    Initial commit

---
+++ Chess_Proj/Chess_Proj.cpp
@@ -0,0 +1,18 @@
+// Chess_Proj.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace Chess_Proj;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
